53b2b5c4d20e8839bf39adf356960d6f3d2e7047
YasmineEl-shahat <yasmenelshahat754@gmail.com>
2023-06-21 14:49:43 +0300

    slider

--- api/home.js
+++ api/home.js
@@ -2,5 +2,5 @@ import { api_url } from "../config/config";
 import { authorizedHttp } from "../config/http";
 
 export async function getHome() {
-  return await authorizedHttp.get(`${api_url}/app/home`);
+  return await authorizedHttp.get(`${api_url}/home`);
 }
--- src/components/Views/HomeComponent.vue
+++ src/components/Views/HomeComponent.vue
@@ -1,15 +1,80 @@
 <template>
   <NavbarComponent />
+  <main class="mainContainer">
+    <div id="slider" class="carousel slide" data-bs-ride="carousel">
+      <ol class="carousel-indicators">
+        <li
+          v-for="(slide, index) in slider"
+          :key="index"
+          :data-bs-target="'#slider'"
+          :data-bs-slide-to="index"
+          :class="{ active: index === 0 }"
+        ></li>
+      </ol>
+      <div class="carousel-inner">
+        <div
+          v-for="(slide, index) in slider"
+          :key="slide.id"
+          :class="{ active: index === 0 }"
+          class="carousel-item"
+        >
+          <a :href="slide.link" target="_blank">
+            <img :src="slide.image" class="d-block w-100" alt="Slide Image" />
+            <div class="carousel-caption d-none d-md-block">
+              <h5>{{ slide.name }}</h5>
+            </div>
+          </a>
+        </div>
+      </div>
+      <a
+        class="carousel-control-prev"
+        href="#slider"
+        role="button"
+        data-bs-slide="prev"
+      >
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a
+        class="carousel-control-next"
+        href="#slider"
+        role="button"
+        data-bs-slide="next"
+      >
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+  </main>
   <FooterComponent />
 </template>
 
 <script>
 import NavbarComponent from "../NavbarComponent.vue";
 import FooterComponent from "../FooterComponent.vue";
+import { getHome } from "../../../api/home";
+
 export default {
   name: "HomeComponent",
   components: { NavbarComponent, FooterComponent },
+  data() {
+    return { slider: [], error: "" };
+  },
+  async created() {
+    try {
+      const result = await getHome();
+      console.log(result.data.data);
+      this.slider = result.data.data.slider;
+    } catch (error) {
+      this.error = error.response.data.message;
+    }
+  },
+  methods: {},
 };
 </script>
 
-<style></style>
+<style>
+.carousel-inner {
+  max-height: 50vh;
+}
+</style>
